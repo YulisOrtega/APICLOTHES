a1a3637b51c93365ffa3b99e201c3cd8f21b4f04
YulisOrtega <ortegayulissa423@gmail.com>
2024-02-23 00:21:08 -0600

    Subiendo trabajos al repositorio

--- src/index.js
+++ src/index.js
@@ -1,34 +1,5 @@
-// Importar el módulo de Express
-const express = require('express');
-
-// Crear una aplicación web con Express
-const app = express();
-
-// Definir una función que calcule el factorial de un número
-/*function factorial(n) {
-  // Si n es menor o igual que 1, el factorial es 1
-  if (n <= 1) {
-    return 1;
-  }
-  // Si no, el factorial es n multiplicado por el factorial de n-1
-  else {
-    return n * factorial(n - 1);
-  }
-}
-
-
-app.get('/factorial/:n', (req, res) => {
-  // Obtener el valor del parámetro n
-  let n = parseInt(req.params.n);
-  // Calcular el factorial de n
-  let f = factorial(n);
-  res.json({
-    status:`El factorial de ${n} es ${f}`
-  });
-});
-*/
-// Iniciar un servidor web en el puerto 3500
-app.listen(3500, () => {
-  console.log('Server listening on port 3500');
-});
-
+import app from "./app.js";
+import database from "./database.js"
+app.listen(process.env.PORT, ()=>{
+  console.log('Server on port' + process.env.PORT);
+})
---
+++ src/models/clothes.model.js
@@ -0,0 +1,19 @@
+import { Schema, model } from "mongoose";
+const productSchema= new Schema({
+    barcode:{
+        type: String,
+        unique: true,
+        require: true
+    },
+    description: String,
+    size: String,
+    brand: String,
+    price: Number,
+    stock: Number,
+    status: Number
+}, {
+    versionKey:false,//para que no se inserte en los documentos "_versionV"
+    timestamps: true,
+});
+
+export default model('product', productSchema);//para exportar el producto y utilizar el esquema "productSchema"
